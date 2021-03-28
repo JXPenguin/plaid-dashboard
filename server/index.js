35028dbe08fd7e140e0317139d9ffbdaae7af5ab
JXPenguin <43251234+JXPenguin@users.noreply.github.com>
2021-03-28 19:39:32 -0400

    updated package.json and server to handle heroku deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const path = require("path");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
@@ -28,6 +29,16 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/plaid", plaid);
+
+// Server static assets if in prod
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
